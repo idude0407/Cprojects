76df99e2156950b2b6ea689c758b884ad21b4c8d
idude0407 <idude0407@gmail.com>
2023-09-01 00:32:52 +0100

    Deleted old files

--- Get Input/main.c
+++ Get Input/main.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+    /*
     char name[30];
     int age;
     char grade;
@@ -16,6 +17,25 @@ int main()
     scanf("%c", &grade);
     printf("What is your gpa? ");
     scanf("%lf", &gpa);
-    printf("Your name is %s and you are %d years old\nYour grade is %c and your gpa is %f", name, age, grade, gpa);
+    printf("Your name is %s and you are %d years old.", name, age);
+    printf("Your grade is %c and your gpa is %f.", grade, gpa);
+    return 0;*/
+    char name[30];
+    int age;
+    char grade;
+    double gpa;
+
+    printf("Enter your name: ");
+    fgets(name, 30, stdin);
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your name is %s and your age is %d.\n", name, age);
+
+    printf("Enter your grade: ");
+    scanf("%c", grade);
+    printf("Enter your GPA: ");
+    scanf("%lf", &gpa);
+    printf("Your grade is %c and your GPA is %f", grade, gpa);
     return 0;
+
 }
