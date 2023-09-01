083dbc60dc74cfbe774c9bc6217b044c2f4d1c30
idude0407 <idude0407@gmail.com>
2023-09-01 14:19:56 +0100

    Fixed getinput

--- Get Input/main.c
+++ Get Input/main.c
@@ -3,39 +3,19 @@
 
 int main()
 {
-    /*
-    char name[30];
+    char name[20];
     int age;
-    char grade;
-    double gpa;
-
-    printf("What is your name? ");
-    fgets(name, 30, stdin);
-    printf("Enter your age: ");
-    scanf("%d", &age);
-    printf("What is your grade? ");
-    scanf("%c", &grade);
-    printf("What is your gpa? ");
-    scanf("%lf", &gpa);
-    printf("Your name is %s and you are %d years old.", name, age);
-    printf("Your grade is %c and your gpa is %f.", grade, gpa);
-    return 0;*/
-    char name[30];
-    int age;
-    char grade;
+    char grading;
     double gpa;
 
     printf("Enter your name: ");
-    fgets(name, 30, stdin);
+    fgets(name, 20, stdin); // All character will start in a new line
     printf("Enter your age: ");
     scanf("%d", &age);
-    printf("Your name is %s and your age is %d.\n", name, age);
-
     printf("Enter your grade: ");
-    scanf("%c", grade);
+    scanf(" %c", &grading); // Must have a space before the specifier
     printf("Enter your GPA: ");
     scanf("%lf", &gpa);
-    printf("Your grade is %c and your GPA is %f", grade, gpa);
+    printf("Your name is %sYour age is %d\nYour grade is %c\nYour GPA is %.1f\n", name, age, grading, gpa);
     return 0;
-
 }
